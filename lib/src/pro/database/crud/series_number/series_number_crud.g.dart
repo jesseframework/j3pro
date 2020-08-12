87ac6489ac138bb6e2a78dbeb6f73c9d755c7392
jesseframework <jgray@cpxzgroup.com>
2020-08-11 20:52:14 -0500

    JG #JF-216 Add Serias Number Generator

---
+++ lib/src/pro/database/crud/series_number/series_number_crud.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'series_number_crud.dart';
+
+// **************************************************************************
+// DaoGenerator
+// **************************************************************************
+
+mixin _$SeriesNumberGeneratorDaoMixin on DatabaseAccessor<AppDatabase> {
+  $SeriesNumberGeneratorTable get seriesNumberGenerator =>
+      attachedDatabase.seriesNumberGenerator;
+}
