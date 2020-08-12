9a2467e83dcf9ae9d8c0ce9f69130e5f63cb2385
Jesse Framework <61049847+jesseframework@users.noreply.github.com>
2020-08-12 14:49:48 -0500

    JG #JF-201 Create customer address (#7)

--- lib/src/pro/models/sales/fullfillment/journey_plan.dart
+++ lib/src/pro/models/sales/fullfillment/journey_plan.dart
@@ -2,7 +2,7 @@ import 'package:j3enterprise/src/resources/shared/extension/full_audited.dart';
 import 'package:j3enterprise/src/resources/shared/extension/must_have_tenant.dart';
 import 'package:moor/moor.dart';
 
-class Customer extends Table implements MustHaveTenant, FullAudited {
+class JourneyPlan extends Table implements MustHaveTenant, FullAudited {
   IntColumn get id => integer()();
   IntColumn get tenantId => integer().nullable()();
   TextColumn get customerId => text()();
@@ -11,25 +11,12 @@ class Customer extends Table implements MustHaveTenant, FullAudited {
   TextColumn get customerType => text()();
   TextColumn get customerGroup => text()();
   TextColumn get customerTerritory => text()();
-  TextColumn get defaultCurrency => text()();
-  TextColumn get paymentTerms => text()();
-  TextColumn get language => text()();
-  RealColumn get creditLimit => real()();
   TextColumn get billingAddressName => text().nullable()();
   TextColumn get shippingAddressName => text().nullable()();
-  TextColumn get contactName => text().nullable()();
-  TextColumn get priceList => text().nullable()();
-  RealColumn get minQuantity => real()();
-  RealColumn get maxQuantity => real()();
-  TextColumn get discountType => text()();
-  RealColumn get discountPercentage => real()();
-  RealColumn get discountAmount => real()();
-  RealColumn get enableHeaderDiscount => real()();
-  RealColumn get accumulatedPurchase => real()();
-  DateTimeColumn get validFrom => dateTime()();
-  DateTimeColumn get validTo => dateTime()();
-  TextColumn get taxId => text()();
-  TextColumn get taxGroup => text()();
+  TextColumn get assignTo => text().nullable()();
+  DateTimeColumn get expiryDate => dateTime().nullable()();
+  IntColumn get weekNumber => integer().nullable()();
+  TextColumn get weekDay => text().nullable()();
   IntColumn get createUserId => integer().nullable()();
   DateTimeColumn get creationTime => dateTime().nullable()();
   DateTimeColumn get deleteTime => dateTime().nullable()();
