221a0e2289556b77356522e5a70e7a85f09b04cf
jesseframework <jgray@cpxzgroup.com>
2020-08-25 10:49:20 -0500

    JG #Dev Sales Order Detail

---
+++ lib/src/pro/database/crud/sales/sales_order/sales_order_detail_temp_crud.g.dart
@@ -0,0 +1,9 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sales_order_detail_temp_crud.dart';
+
+// **************************************************************************
+// DaoGenerator
+// **************************************************************************
+
+mixin _$SalesOrderDetailtempDaoMixin on DatabaseAccessor<AppDatabase> {}
